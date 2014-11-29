0e56f7f405389d82cbfa6ab0e48da4e68083139a
Julius Beckmann <github@h4cc.de>
2014-11-29 11:10:17 +0100

    some code

---
+++ web/index.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Dropbox\Client;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Dropbox as Adapter;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once(__DIR__.'/../config/config.php');
+
+$client = new Client($token, $appName);
+$filesystem = new Filesystem(new Adapter($client, ''));
+
+$github = new Github\Client();
+$github->authenticate($githubToken, 'http_token');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app['github'] = $github;
+$app['dropbox.client'] = $client;
+$app['storage'] = $filesystem;
+
+$app->get('/', function() use($app) {
+    return 'This is a example composer dist storage system.';
+});
+
+$app->get('/{user}/{repo}/zipball/{ref}', function($user, $repo, $ref) use($app) {
+
+    $path = $user.'__'.$repo.'__'.$ref;
+
+    if(!$app['storage']->has($path)) {
+
+        $content = $app['github']->api('repo')->contents()->archive($user, $repo, 'zipball', $ref);
+
+        if(!$content) {
+            return new Response('Could not fetch content from github', 500);
+        }
+
+        $result = $app['storage']->write($path, $content);
+
+        if(!$result) {
+            return new Response('Could not write to storage', 500);
+        }
+    }
+
+    $link = $app['dropbox.client']->createShareableLink('/'.$path);
+
+    return $app->json([
+        'link' => $link
+    ]);
+});
+
+$app->run(); 
